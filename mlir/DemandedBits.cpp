6583111ed75fa806e03cea17adc1c6e1897572bb
spica <spicaofsky@gmail.com>
2024-11-07 18:55:08 -0800

    rocket.mlir test added

---
+++ mlir/DemandedBits.cpp
@@ -0,0 +1,5 @@
+//
+// Created by spica on 9/12/24.
+//
+
+#include "DemandedBits.h"
---
+++ mlir/DemandedBits.h
@@ -0,0 +1,14 @@
+//
+// Created by spica on 9/12/24.
+//
+
+#ifndef PLAYWITHLLVM_DEMANDEDBITS_H
+#define PLAYWITHLLVM_DEMANDEDBITS_H
+
+
+class DemandedBits {
+
+};
+
+
+#endif //PLAYWITHLLVM_DEMANDEDBITS_H
